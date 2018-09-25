8aac5562eaa4382b467913391f965f639090f2b7
Chris Davis <christopherjdavis417@gmail.com>
2018-09-25 07:40:54 -0400

    Add project files.

---
+++ BLRestaurants/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLRestaurants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "restaurant.csv");
+            var fileContents = ReadRestaurants(fileName);
+        }
+
+        public static List<Restaurant> ReadRestaurants(string fileName)
+        {
+            var listRestaurants = new List<Restaurant>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                reader.ReadLine();
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var restaurant = new Restaurant();
+                    string[] values = line.Split(',');
+
+                    int ID;
+                    if (int.TryParse(values[0], out ID))
+                    {
+                        restaurant.ID = ID;
+                    }
+
+                    listRestaurants.Add(restaurant);
+                }
+            }
+
+
+                return listRestaurants;
+        }
+    }
+}
---
+++ BLRestaurants/Restaurant.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLRestaurants
+{
+    class Restaurant
+    {
+        public int ID { get; set; }
+        public int RestaurantName { get; set; }
+        public BLType Type { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+
+        public enum BLType
+        {
+            Buffet = 1,
+            Brunch = 2 
+        }
+    }
+}
