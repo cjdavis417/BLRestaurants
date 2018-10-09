82e73def8152f087fa59b95a6a691ac8e3083504
Chris Davis <christopherjdavis417@gmail.com>
2018-10-09 19:05:51 -0400

    edit app smartness :-)

--- BLRestaurants/Program.cs
+++ BLRestaurants/Program.cs
@@ -9,17 +9,46 @@ namespace BLRestaurants
 {
     class Program
     {
+        public List<string> ListOfCommands = new List<string> { "Help" };
+
         static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Brunch Louisville");
+            Console.WriteLine("Type 'help' at any point to see all available commands.");
+            ListRestaurants();
+
+        }
+
+
+
+        void Commands(List<string> commands)
+        {
+            foreach (var command in commands)
+            {
+                Console.WriteLine(command);
+            }
+        }
+
+        public static void ListRestaurants()
         {
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "restaurants.csv");
             var fileContents = ReadRestaurants(fileName);
+
+            foreach (var content in fileContents)
+            {
+                Console.WriteLine(content.RestaurantName);
+                Console.WriteLine(content.Restaurant_Type);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
         }
 
         public static List<Restaurant> ReadRestaurants(string fileName)
         {
-            var listRestaurants = new List<Restaurant>();
+            var Restaurants = new List<Restaurant>();
             using (var reader = new StreamReader(fileName))
             {
                 string line = "";
@@ -34,24 +63,29 @@ namespace BLRestaurants
                     {
                         restaurant.ID = intParse;
                     }
-                    restaurant.RestaurantName = values[1];
+                    if (int.TryParse(values[1], out intParse))
+                    {
+                        restaurant.Restaurant_ID = intParse;
+                    }
+                    restaurant.RestaurantName = values[2];
+                    restaurant.Restaurant_Type = values[3];
 
                     double doubleParse;
-                    if (double.TryParse(values[3], out doubleParse))
+                    if (double.TryParse(values[4], out doubleParse))
                     {
                         restaurant.Longitude = doubleParse;
                     }
-                    if (double.TryParse(values[4], out doubleParse))
+                    if (double.TryParse(values[5], out doubleParse))
                     {
                         restaurant.Latitude = doubleParse;
                     }
 
-                    listRestaurants.Add(restaurant);
+                    Restaurants.Add(restaurant);
                 }
             }
 
 
-                return listRestaurants;
+                return Restaurants;
         }
     }
 }
--- BLRestaurants/Restaurant.cs
+++ BLRestaurants/Restaurant.cs
@@ -9,15 +9,13 @@ namespace BLRestaurants
     class Restaurant
     {
         public int ID { get; set; }
+        public int Restaurant_ID { get; set; }
         public string RestaurantName { get; set; }
-        public BLType Type { get; set; }
+        public string Restaurant_Type { get; set; }
+        public string Type { get; set; }
         public double Longitude { get; set; }
         public double Latitude { get; set; }
 
-        public enum BLType
-        {
-            Buffet = 1,
-            Brunch = 2 
-        }
+        
     }
 }
